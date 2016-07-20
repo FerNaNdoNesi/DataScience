7bd7ff4636d905899786dfa2ae4be7fdf4c1747d
Fernando Nesi <fernando.nesi@cresolsicoper.com.br>
2016-07-20 16:28:00 -0300

    Primeiros arquivos

---
+++ RFundamentos/parte01/R01.R
@@ -0,0 +1,32 @@
+#Explorando o R01
+
+#Nome dos contributors
+contributors()
+license()
+
+#Informações sobre a seção
+sessionInfo()
+
+#Imprimir na tela
+print('Print em R')
+
+#Gerar Gráfico
+plot(1:30)
+
+#Instalar pacotes
+install.packages('randomForest')
+install.packages('ggplot2')
+
+#Carregar pacotes instalados
+library(ggplot2) #Para ver pacores carregados Painel inferior-esquedo > packeage
+
+#Descarregar Pacote
+detach(package:ggplot2)
+
+#Saber pelo NOME como a função funciona (busca dentro do manual do R)
+help(mean)
+?mean
+
+#Se não souber o nome da função (Busca todas as referencias sobre)
+help.search('randomForest')
+help.search('mathplot')
