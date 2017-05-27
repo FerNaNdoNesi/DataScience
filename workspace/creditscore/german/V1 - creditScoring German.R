1e5505b86db831c268b7130b66d3150b00108708
FerNaNdoNesi <fernandonesi@gmail.com>
2017-05-27 19:51:06 -0300

    Resultados Geman completos 01

--- workspace/creditscore/german/V1 - creditScoring German.R
+++ workspace/creditscore/german/V1 - creditScoring German.R
@@ -1,3 +1,15 @@
+dbn_model <- darch(train.x,
+                   train.y,
+                   rbm.numEpochs = 0,
+                   rbm.batchSize = 100,
+                   rbm.trainOutputLayer = F,
+                   layers = c(21,100,2),
+                   darch.batchSize = 100,
+                   darch.learnRate = 2,
+                   darch.retainData = F,
+                   darch.numEpochs = 20
+                  )
+
 install.packages("readr") # Usando o pacote readr
 install.packages("dplyr") #tratamento de dados em memória
 install.packages("tidyr") # Objetivo de remodelar os dados
@@ -30,6 +42,62 @@ glimpse(arq_german_credit_data) #Visualização rápida
 ######################### ETAPA 01: Limpeza dos dados #########################
 any(is.na(arq_german_credit_data)) # se existir valores nulos é necessário trata-los
 
+
+
+######################### Valores categóricos em numéricos ######################### 
+arq_german_credit_data <- read_csv("german-credit-data.csv") #Carrega dados
+mod_arq_german_credit_data <- read_csv("german-credit-data.csv") #Carrega dados
+glimpse(arq_german_credit_data)
+glimpse(mod_arq_german_credit_data)
+table(arq_german_credit_data$StatusCc)
+summary(arq_german_credit_data$StatusCc)
+prop.table(table(arq_german_credit_data$StatusCc))
+scale(arq_german_credit_data$PercentRend)
+mod_arq_german_credit_data$StatusCc <- factor(arq_german_credit_data$StatusCc, levels = c("A11", "A12", "A13", "A14"), labels = c(1.0, 2.0, 3.0, 4.0)) #Classe como fator
+arq_german_credit_data$class <- factor(arq_german_credit_data$class, levels = c(1,2), labels = c("Bom", "Ruim")) #Classe como fator
+
+dados_norm <- as.data.frame(lapply(arq_german_credit_data$MesesCc, numer))
+glimpse(mod_arq_german_credit_data)
+summary(dados_norm$MesesCc)
+#dados_norm <- as.data.frame(lapply(arq_german_credit_data, normalizar))
+#glimpse(dados_norm)
+
+#INICIO
+arq_german_credit_data <- read_csv("german-credit-data.csv") #Carrega dados
+mod_arq_german_credit_data <- read_csv("german-credit-data.csv") #Carrega dados
+#glimpse(mod_arq_german_credit_data)
+mod_arq_german_credit_data$StatusCc <- as.double(factor(arq_german_credit_data$StatusCc, levels = labels(c(table(arq_german_credit_data$StatusCc))), labels = c(1:length(c(table(arq_german_credit_data$StatusCc))))))
+mod_arq_german_credit_data$MesesCc <- as.double(mod_arq_german_credit_data$MesesCc)
+mod_arq_german_credit_data$HistCredit <- as.double(factor(arq_german_credit_data$HistCredit, levels = labels(c(table(arq_german_credit_data$HistCredit))), labels = c(1:length(c(table(arq_german_credit_data$HistCredit))))))
+mod_arq_german_credit_data$PropEmp <- as.double(factor(arq_german_credit_data$PropEmp, levels = labels(c(table(arq_german_credit_data$PropEmp))), labels = c(1:length(c(table(arq_german_credit_data$PropEmp))))))
+mod_arq_german_credit_data$MontEmp <- as.double(mod_arq_german_credit_data$MontEmp)
+mod_arq_german_credit_data$Invest <- as.double(factor(arq_german_credit_data$Invest, levels = labels(c(table(arq_german_credit_data$Invest))), labels = c(1:length(c(table(arq_german_credit_data$Invest))))))
+mod_arq_german_credit_data$TrabAtual <- as.double(factor(arq_german_credit_data$TrabAtual, levels = labels(c(table(arq_german_credit_data$TrabAtual))), labels = c(1:length(c(table(arq_german_credit_data$TrabAtual))))))
+mod_arq_german_credit_data$PercentRend <- as.double(mod_arq_german_credit_data$PercentRend)
+mod_arq_german_credit_data$EstCivil <- as.double(factor(arq_german_credit_data$EstCivil, levels = labels(c(table(arq_german_credit_data$EstCivil))), labels = c(1:length(c(table(arq_german_credit_data$EstCivil))))))
+mod_arq_german_credit_data$OutrosDev <- as.double(factor(arq_german_credit_data$OutrosDev, levels = labels(c(table(arq_german_credit_data$OutrosDev))), labels = c(1:length(c(table(arq_german_credit_data$OutrosDev))))))
+mod_arq_german_credit_data$ResidAtual <- as.double(mod_arq_german_credit_data$ResidAtual)
+mod_arq_german_credit_data$TipoResid <- as.double(factor(arq_german_credit_data$TipoResid, levels = labels(c(table(arq_german_credit_data$TipoResid))), labels = c(1:length(c(table(arq_german_credit_data$TipoResid))))))
+mod_arq_german_credit_data$Idade <- as.double(mod_arq_german_credit_data$Idade)
+mod_arq_german_credit_data$OutrosParc <- as.double(factor(arq_german_credit_data$OutrosParc, levels = labels(c(table(arq_german_credit_data$OutrosParc))), labels = c(1:length(c(table(arq_german_credit_data$OutrosParc))))))
+mod_arq_german_credit_data$TipoMoradia <- as.double(factor(arq_german_credit_data$TipoMoradia, levels = labels(c(table(arq_german_credit_data$TipoMoradia))), labels = c(1:length(c(table(arq_german_credit_data$TipoMoradia))))))
+mod_arq_german_credit_data$NumEmpAtivo <- as.double(mod_arq_german_credit_data$NumEmpAtivo)
+mod_arq_german_credit_data$TipoTrab <- as.double(factor(arq_german_credit_data$TipoTrab, levels = labels(c(table(arq_german_credit_data$TipoTrab))), labels = c(1:length(c(table(arq_german_credit_data$TipoTrab))))))
+mod_arq_german_credit_data$NumPessoas <- as.double(mod_arq_german_credit_data$NumPessoas)
+mod_arq_german_credit_data$Telefone <- as.double(factor(arq_german_credit_data$Telefone, levels = labels(c(table(arq_german_credit_data$Telefone))), labels = c(1:length(c(table(arq_german_credit_data$Telefone))))))
+mod_arq_german_credit_data$TrabEstr <- as.double(factor(arq_german_credit_data$TrabEstr, levels = labels(c(table(arq_german_credit_data$TrabEstr))), labels = c(1:length(c(table(arq_german_credit_data$TrabEstr))))))
+glimpse(mod_arq_german_credit_data)
+dados_norm <- as.data.frame(lapply(mod_arq_german_credit_data, scale))
+glimpse(dados_norm)
+dados_norm$class <- factor(dados_norm$class, levels = as.double(labels(c(table(dados_norm$class)))), labels = c("Bom", "Ruim")) #Classe como fator
+mod_arq_german_credit_data$class <- factor(arq_german_credit_data$class, levels = c(1,2), labels = c("Bom", "Ruim")) #Classe como fator
+arq_german_credit_data$class <- factor(arq_german_credit_data$class, levels = c(1,2), labels = c("Bom", "Ruim")) #Classe como fator
+
+
+
+glimpse(mod_arq_german_credit_data)
+glimpse(arq_german_credit_data)
+glimpse(dados_norm)
 
 ####################### ETAPA 02: Análise exploratória ########################
 #1 - Atributo classe deve ser do tipo Fator
@@ -82,6 +150,9 @@ hist(arq_german_credit_data$MontEmp, main = "Histograma para montante de emprest
 #Detectar problemas de escala entre os dados distâncias muito distintas precisam ser normalizadas
 normalizar <- function(x){
   return ((x - min(x)) / (max(x) - min(x)))
+}
+numer <- function(x){
+  return (x * 1.0)
 }
 dados_norm <- as.data.frame(lapply(arq_german_credit_data[c("MesesCc", "MontEmp", "Idade")], normalizar))
 head(dados_norm)
@@ -271,22 +342,24 @@ df_mat <- data.frame( Category = c("Credito Ruim", "Credito Bom"),
 print(df_mat)
 ################# RESUMINDO DECISION TREE: Treinando e Testando Modelo #################FIM
 ################# RESUMINDO RANDOM FOREST: Treinando e Testando Modelo #################INICIO
-arq_german_credit_data <- read_csv("german-credit-data.csv") #Carrega dados
-arq_german_credit_data$class <- factor(arq_german_credit_data$class, levels = c(1,2), labels = c("Good", "Bad")) #Classe como fator
-set.seed(6374) 
+
+library(C50)
 AuxMat <- matrix(nrow = 13, ncol = 11)
 dimnames(AuxMat) = (list( c("T01","T02","T03","T04","T05","T06","T07","T08","T09","T10","Média","Mediana","DesvioP"),
                           c("acertos","erros","VP","FN","FP","VN","Precisão","recall","acuracia","F1","AUC")))
-
+glimpse(arq_german_credit_data)
+glimpse(mod_arq_german_credit_data)
+glimpse(dados_norm)
+dataset <- arq_german_credit_data
 Cost_func <- matrix(c(0.0, 4.5, 3.0, 0.0), nrow = 2, dimnames = list(c("Good", "Bad"), c("Good", "Bad")))
 for (i in 1:10) {
   #set.seed(6374) 
-  amostra <- sample.split(arq_german_credit_data, SplitRatio = 0.70) #Split dataSet
-  dados_treino = subset(arq_german_credit_data, amostra == TRUE) #DataSet Treino
-  dados_teste = subset(arq_german_credit_data, amostra == FALSE) #DataSet Testte
+  amostra <- sample.split(dataset, SplitRatio = 0.70) #Split dataSet
+  dados_treino = subset(dataset, amostra == TRUE) #DataSet Treino
+  dados_teste = subset(dataset, amostra == FALSE) #DataSet Testte
   #modelo_floresta <- rpart(class ~ . , method = 'class', data = dados_treino) #Treino Modelo
-  modelo_floresta <- C5.0( class ~ ., data = dados_treino, ntree = 100, nodesize = 10, cost = Cost_func)
-  #modelo_floresta <- C5.0( class ~ ., data = dados_treino, ntree = 750, nodesize = 10)
+  #modelo_floresta <- C5.0( class ~ ., data = dados_treino, ntree = 100, nodesize = 10, cost = Cost_func)
+  modelo_floresta <- C5.0( class ~ ., data = dados_treino, ntree = 750, nodesize = 10)
   test_forest_predict = predict(modelo_floresta, newdata = dados_teste, type = "class"); #Testa Modelo
   CT <- CrossTable(x = test_forest_predict, y = dados_teste$class, prop.chisq = FALSE) ## confusion matrix
   acerto <- mean(dados_teste$class == test_forest_predict)*100 #Calculando a taxa de acerto
@@ -310,6 +383,20 @@ AuxMat[i+3,j] = sd(AuxMat[1:10,j])
 }
 AuxMat
 View(AuxMat)
+
+darch <- darch(train.x,
+               train.y,
+               rbm.numEpochs = 0,
+               rbm.batchSize = 100,
+               rbm.trainOutputLayer = F,
+               layers = c(21,100,2),
+               darch.batchSize = 100,
+               darch.learnRate = 2,
+               darch.retainData = F,
+               darch.numEpochs = 20
+              )
+
+
 ################# RESUMINDO RANDOM FOREST: Treinando e Testando Modelo #################FIM
 
 ## Otimizando o modelo && utilizando Floresta
@@ -511,29 +598,33 @@ prp(dbn_model)
 ################# RESUMINDO REDE DE CRENÇA PROFUNDA: Treinando e Testando Modelo #################INICIO
 install.packages("darch")
 library(darch)
-arq_german_credit_data <- read_csv("credit-dataset-num.csv") #Carrega dados
 glimpse(arq_german_credit_data)
-arq_german_credit_data$class <- factor(arq_german_credit_data$class, levels = c(1,2), labels = c(1, 2)) ##c("Bom", "Ruim")) #Classe como fator
-dados_norm$class <- factor(arq_german_credit_data$class, levels = c(1,2), labels = c("Bom", "Ruim"))
+glimpse(mod_arq_german_credit_data)
+glimpse(dados_norm)
+
 table(arq_german_credit_data$class)
+table(mod_arq_german_credit_data$class)
 table(dados_norm$class)
+
 AuxMat <- matrix(nrow = 13, ncol = 11)
 dimnames(AuxMat) = (list( c("T01","T02","T03","T04","T05","T06","T07","T08","T09","T10","Média","Mediana","DesvioP"),
                           c("acertos","erros","VP","FN","FP","VN","Precisão","recall","acuracia","F1","AUC")))
-for (i in 1:4) {
+dataset <- dados_norm
+for (i in 1:10) {
   #dados_norm <- as.data.frame(lapply(arq_german_credit_data, normalizar))
+  #dados_norm <- as.data.frame(lapply(arq_german_credit_data, scale))
   #glimpse(dados_norm)
   
-  #arq_german_credit_data <- dados_norm
-  amostra <- sample.split(arq_german_credit_data, SplitRatio = 0.70) #Split dataSet
-  dados_treino = subset(arq_german_credit_data, amostra == TRUE) #DataSet Treino
-  dados_teste = subset(arq_german_credit_data, amostra == FALSE) #DataSet Testte
+  
+  amostra <- sample.split(dataset, SplitRatio = 0.70) #Split dataSet
+  dados_treino = subset(dataset, amostra == TRUE) #DataSet Treino
+  dados_teste = subset(dataset, amostra == FALSE) #DataSet Testte
   #glimpse(dados_treino)
   #glimpse(dados_teste)
   #modelo_arvore <- rpart(class ~ . , method = 'class', data = dados_treino) #Treino Modelo
   #modelo_floresta <- C5.0( class ~ ., data = dados_treino, ntree = 750, nodesize = 10)
-  dbn_model <- darch(class ~ ., data = dados_treino, layers = c(4, 10, 2),
-                     darch.numEpochs = 10, retainData = T) #Treino Modelo
+  dbn_model <- darch(class ~ ., data = dados_treino, layers = c(20, 10, 2),
+                     darch.numEpochs = 10, darch.stopClassErr = 0, retainData = T) #Treino Modelo
   test_dbn_predict <- predict(dbn_model, newdata = dados_teste, type = "class"); #Testa Modelo
   CT <- CrossTable(x = test_dbn_predict, y = dados_teste$class, prop.chisq = FALSE) ## confusion matrix
   acerto <- mean(dados_teste$class == test_dbn_predict)*100 #Calculando a taxa de acerto
@@ -551,9 +642,9 @@ for (i in 1:4) {
   AuxMat[i,11] = AUC(CT$t)
 }
 for (j in 1:11) {
-  AuxMat[i+1,j] = mean(AuxMat[1:4,j])
-  AuxMat[i+2,j] = median(AuxMat[1:4,j])
-  AuxMat[i+3,j] = sd(AuxMat[1:4,j])
+  AuxMat[i+1,j] = mean(AuxMat[1:10,j])
+  AuxMat[i+2,j] = median(AuxMat[1:10,j])
+  AuxMat[i+3,j] = sd(AuxMat[1:10,j])
 }
 AuxMat
 View(AuxMat)
